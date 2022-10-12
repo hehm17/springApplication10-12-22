2d30f03440451d465a474cce3f71710e3afa9903
hehm17 <113297349+hehm17@users.noreply.github.com>
2022-10-12 17:46:58 +0530

    Add files via upload

---
+++ AssingnmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Assingnment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssingnmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssingnmentApplication.class, args);
+	}
+
+}
